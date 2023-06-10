6c8e5dcecc7dd29e02ad7f42ea74e905d01f66d3
aspyasa <nadillastia@outlook.com>
2023-06-10 15:26:40 +0700

    atras 2 commit

--- src/main/java/com/example/productservice/model/Product.java
+++ src/main/java/com/example/productservice/model/Product.java
@@ -1,14 +1,16 @@
 package com.example.productservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 @Data
 @Builder
 @Table(name = "PRODDUCT_TABLE")
